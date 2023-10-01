82b8f2908ab0b9ceb05633667d9956f431d4db65
myos911 <myos911@aucklanduni.ac.nz>
2023-10-02 11:28:42 +1300

    Added automated tests for each step

--- test_output.py
+++ test_output.py
@@ -5,6 +5,7 @@ import numpy as np
 
 import unittest
 import subprocess
+import argparse
 
 
 
@@ -20,20 +21,70 @@ def load_dump(file_path: str):
 class TestRangeDoppler(unittest.TestCase):
 
     @classmethod
-    def setUp(self):
-        
+    def setUpClass(cls):
         #  Setup the original data
-        self.original_step1_matched_filter = load_dump('./original_data/matched_filter.pk')
-        self.original_step2_azimuth_fft = load_dump('./original_data/azimuth_fft.pk')
-        self.original_step3_rcmc = load_dump('./original_data/rcmc.pk')
-        self.original_step4_azmiuth_filter = load_dump('./original_data/azimuth_filter4.pk')
-        self.original_step5_azmiuth_ifft = load_dump('./original_data/azimuth_ifft.pk')
-        print('Running rangeDoppler for dumps... Please wait...')
-        result = subprocess.run(["python3.8", "scriptedsympyr.py", "--dump-data"], capture_output=True, text=True)
-        print('Success: ', result.returncode == 0 )
+        cls.original_step1_matched_filter = load_dump('./original_data/matched_filter.pk')
+        cls.original_step2_azimuth_fft = load_dump('./original_data/azimuth_fft.pk')
+        cls.original_step3_rcmc = load_dump('./original_data/rcmc.pk')
+        cls.original_step4_azmiuth_filter = load_dump('./original_data/azimuth_filter4.pk')
+        cls.original_step5_azmiuth_ifft = load_dump('./original_data/azimuth_ifft.pk')
+        
+        cls.tolerance = 1E-6 # Set the tolerance threshold (e.g., 1E-6)
+        
+        # Run the algorithm
+        is_running_algorithm = False
+        if is_running_algorithm:
+            print('Running algorithm... Please wait...')
+            result = subprocess.run(["python3.8", "scriptedsympyr.py", "--dump-data"], capture_output=True, text=True)
+            print('SUCCESSFUL RUN?: ', result.returncode == 0)
+        else:
+            print('Algorithm execution is disabled. Using most recent retrace_data dump')
 
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+    def test_step1_matched_filter(self):
+
+        # Load the dump
+        retrace_step1_matched_filter = load_dump('./retrace_data/matched_filter.pk')
+
+        absolute_difference = np.abs(retrace_step1_matched_filter - self.original_step1_matched_filter)
+
+        # Check if all elements in the relative difference array are within the tolerance
+        self.assertEqual(retrace_step1_matched_filter.shape, self.original_step1_matched_filter.shape)
+        self.assertTrue(np.all(absolute_difference <= self.tolerance))
+    
+    def test_step2_matched_filter(self):
+
+        # Load the dump
+        retrace_step2_azimuth_fft = load_dump('./retrace_data/azimuth_fft.pk')
+        absolute_difference = np.abs(retrace_step2_azimuth_fft - self.original_step2_azimuth_fft)
+        # Check if all elements in the relative difference array are within the tolerance
+        self.assertEqual(retrace_step2_azimuth_fft.shape, self.original_step2_azimuth_fft.shape)
+        self.assertTrue(np.all(absolute_difference <= self.tolerance))
+
+    def test_step3_rcmc(self):
+
+        # Load the dump
+        retrace_step3_rcmc = load_dump('./retrace_data/rcmc.pk')
+        absolute_difference = np.abs(retrace_step3_rcmc - self.original_step3_rcmc)
+        # Check if all elements in the relative difference array are within the tolerance
+        self.assertEqual(retrace_step3_rcmc.shape, self.original_step3_rcmc.shape)
+        self.assertTrue(np.all(absolute_difference <= self.tolerance))
+    
+    def test_step4_azimuth_filter(self):
+
+        # Load the dump
+        retrace_step4_azmiuth_filter = load_dump('./retrace_data/azimuth_filter4.pk')
+        absolute_difference = np.abs(retrace_step4_azmiuth_filter - self.original_step4_azmiuth_filter)
+        # Check if all elements in the relative difference array are within the tolerance
+        self.assertEqual(retrace_step4_azmiuth_filter.shape, self.original_step4_azmiuth_filter.shape)
+        self.assertTrue(np.all(absolute_difference <= self.tolerance))
+
+    def test_step5_azmiuth_ifft(self):
+        # Load the dump
+        retrace_step5_azmiuth_ifft = load_dump('./retrace_data/azimuth_ifft.pk')
+        absolute_difference = np.abs(retrace_step5_azmiuth_ifft - self.original_step5_azmiuth_ifft)
+        # Check if all elements in the relative difference array are within the tolerance
+        self.assertEqual(retrace_step5_azmiuth_ifft.shape, self.original_step5_azmiuth_ifft.shape)
+        self.assertTrue(np.all(absolute_difference <= self.tolerance))
           
 if __name__ == '__main__':
   unittest.main()
